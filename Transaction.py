f4109cfac18c9d5e9650ff38d5cd839d54bb9eb2
ltanakwebsite <ltanakbusiness@outlook.com>
2024-10-03 16:04:23 +0100

    class atts

--- Transaction.py
+++ Transaction.py
@@ -3,3 +3,15 @@ import numpy as np
 import heapq
 import time
 import math
+
+class Transaction:
+
+    def __init__(self, timestamp, id, type, price, quantity):
+        self.timestamp = timestamp
+        self.id = id
+        self.type = type # "BID / BUY" vs "ASK / SELL"
+        self.price = price
+        self.quantity = quantity
+
+    def generateOrder(self):
+        self.timestamp = time.time()
