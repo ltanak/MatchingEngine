05ddef82e07cdc2ebd39a2c340ea7b538e03af80
ltanakwebsite <ltanakbusiness@outlook.com>
2024-10-04 09:02:22 +0100

    commit

--- MatchingEngine.py
+++ MatchingEngine.py
@@ -1,4 +1,12 @@
 import Transaction
 
 class MatchingEngine:
-    pass
+    
+    def __init__(self, buyBook, sellBook):
+        self.buyBook = buyBook
+        self.sellBook = sellBook
+
+    def priceTimeMatching(self):
+        orderBook = []
+        # TO - DO
+        return orderBook
