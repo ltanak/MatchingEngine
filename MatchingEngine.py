97371e8389388f3b7b22b6e49ab1b830e9ea4e6c
ltanakwebsite <ltanakbusiness@outlook.com>
2024-10-04 13:56:44 +0100

    implementing matching engine

--- MatchingEngine.py
+++ MatchingEngine.py
@@ -1,12 +1,25 @@
-import Transaction
+from Transaction import Transaction
+import heapq
 
 class MatchingEngine:
     
-    def __init__(self, buyBook, sellBook):
-        self.buyBook = buyBook
-        self.sellBook = sellBook
+    def __init__(self):
+        self.buyBook = []   # Both are arrays that will be using heapq
+        self.sellBook = []  # 
 
-    def priceTimeMatching(self):
-        orderBook = []
-        # TO - DO
-        return orderBook
+    def addToBook(self, transaction: Transaction):
+        if transaction.type == "BID":
+            valueToInsert = (transaction.price * -1, transaction.timestamp, transaction)
+            heapq.heappush(self.buyBook, valueToInsert)
+        else:
+            valueToInsert = (transaction.price, transaction.timestamp, transaction)
+            heapq.heappush(self.sellBook, valueToInsert)
+
+    def popFromBuy(self):
+        transaction = heapq.heappop(self.buyBook)
+        transaction[0] *= -1
+        return transaction
+    
+    def popFromSell(self):
+        transaction = heapq.heappop(self.sellBook)
+        return transaction
---
+++ main.py
@@ -0,0 +1,13 @@
+from MatchingEngine import MatchingEngine
+from Transaction import Transaction
+
+a = Transaction(timestamp = 1, price = 50, quantity = 1)
+b = Transaction(timestamp = 2, price = 40, quantity = 90)
+c = Transaction(timestamp = 5, price = 50, quantity = 1)
+d = Transaction(timestamp = 6, price = 100, quantity = 1)
+
+engine = MatchingEngine()
+
+engine.addToBuyBook(a)
+engine.addToBuyBook(b)
+print(engine.buyBook)
