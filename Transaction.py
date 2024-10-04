4e168d98991f9b3708c2c9232cc8d6af7810c276
ltanakwebsite <ltanakbusiness@outlook.com>
2024-10-04 08:46:19 +0100

    transaction generation

--- Transaction.py
+++ Transaction.py
@@ -3,15 +3,21 @@ import numpy as np
 import heapq
 import time
 import math
+import uuid
 
 class Transaction:
 
-    def __init__(self, timestamp, id, type, price, quantity):
+    def __init__(self, generate = False, timestamp = None, id = None, type = "BID", price = None, quantity = None):
         self.timestamp = timestamp
         self.id = id
         self.type = type # "BID / BUY" vs "ASK / SELL"
         self.price = price
         self.quantity = quantity
+        if generate:
+            self.generateOrder()
 
     def generateOrder(self):
         self.timestamp = time.time()
+        self.id = uuid.uuid4()
+        self.price = random.randint(1, 100)
+        self.quantity = random.randint(1, 10000)
