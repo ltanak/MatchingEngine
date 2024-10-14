5e0de03aff6007ff28f94737fa70d09dc5d422ad
ltanakwebsite <ltanakbusiness@outlook.com>
2024-10-14 21:41:47 +0100

    can now quit thread

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ from decimal import Decimal
 
 app = Flask(__name__)
 
+LOCALSTARTTIME = time.time()
+THREADENABLED = True
+
 def background():
     dataSource = "Resources/MSFT1/"
     fileToOpen = dataSource + "MSFTBook.csv"
@@ -22,19 +25,21 @@ def background():
     engine = MatchingEngine()
 
     with open(fileToOpen, newline = "") as csvfile:
-        spamreader = csv.reader(csvfile, delimiter = ",", quotechar= "|")
-        startTime = time.time()  
+        spamreader = csv.reader(csvfile, delimiter = ",", quotechar= "|")  
         for row in spamreader:
-            time.sleep(0.2)
-            row = list(row)
-            if row[1] == "1":
-                transaction = Transaction(fromCSV = row)
-                engine.addToBook(transaction)
-                matched = engine.priceTimePriority()
-                while matched:
-                    # plot.add(time.time() - startTime, transaction.price)
-                    print(time.time() - startTime, transaction.price)
+            if THREADENABLED:
+                time.sleep(0.2)
+                row = list(row)
+                if row[1] == "1":
+                    transaction = Transaction(fromCSV = row)
+                    engine.addToBook(transaction)
                     matched = engine.priceTimePriority()
+                    while matched:
+                        # plot.add(time.time() - startTime, transaction.price)
+                        print(time.time() - LOCALSTARTTIME, transaction.price)
+                        matched = engine.priceTimePriority()
+            else:
+                break
     return "fin"
     
 @app.route('/', methods=["GET", "POST"])
@@ -43,20 +48,21 @@ def main():
 
 @app.route('/data', methods=["GET", "POST"])
 def data():
-    data = [time.time() * 1000, random() * 100]
+    data = [(time.time() - LOCALSTARTTIME) * 1000, random() * 100]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
 @app.route('/matchingData', methods=["GET", "POST"])
 def matchingData(transactionPrice):
-    data = [time.time(), transactionPrice]
+    data = [(time.time() - LOCALSTARTTIME) * 1000, transactionPrice]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
 
 if __name__ == '__main__':
-    trading = threading.Thread(target=background)
-    trading.start()
+    trading = threading.Thread(target=background).start()
     app.run(debug=True, threaded=True)
-    trading.kill()
+    THREADENABLED = False
+    trading.join()
+    exit(0)
