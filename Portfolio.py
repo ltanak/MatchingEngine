9f70284b9bf7d83fe97c32d53aed4b3bce1d0827
ltanakwebsite <ltanakbusiness@outlook.com>
2024-11-01 15:15:33 +0000

    preparing for multiple stocks

---
+++ Portfolio.py
@@ -0,0 +1,23 @@
+from collections import deque
+import random
+import numpy as np
+import heapq
+import time
+import math
+import uuid
+from Transaction import Transaction
+from User import User
+
+class Portfolio:
+
+    def __init__(self, stocks: [User]):
+        self.accounts = {
+            "MSFT": stocks[0],
+            "AAPL": stocks[1], 
+            "AMZN": stocks[2], 
+            "GOOG": stocks[3],
+            "INTC": stocks[4]
+        }
+
+    def getAccount(self, accountCode):
+        return self.accounts[accountCode]
